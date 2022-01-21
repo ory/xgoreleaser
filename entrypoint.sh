c0835b78d2e060e8051efc2663138b38c7fa063d
Anirudh Oppiliappan <x@icyphox.sh>
2022-01-21 16:09:25 +0530

    fix: typo 'ecoh' -> 'echo' (#9)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -n $DOCKER_USERNAME ] && [ -n $DOCKER_PASSWORD ]; then
-    ecoh "Logging in to Docker"
+    echo "Logging in to Docker"
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 fi
 
