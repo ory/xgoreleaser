e3599402ea167ad2af3bf6fceb252a309a8a450c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2023-01-04 11:52:00 +0100

    fix: rectify docker logic

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-ARG GO_VERSION=1.17
+ARG GO_VERSION=1.19
 
 # OS-X SDK parameters
 # NOTE: when changing version here, make sure to also change OSX_CODENAME below to match
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -euxo pipefail
 
-if [ -z ${DOCKER_USERNAME+x} ] && [ -z ${DOCKER_PASSWORD+x} ]; then
+if [ -n "${DOCKER_USERNAME+x}" ] && [ -n "${DOCKER_PASSWORD+x}" ]; then
     echo "Logging in to Docker"
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 fi
